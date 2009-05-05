c0a46567b208e03e9864b81adf88ab780878e272
Radovan Lozej <radovan.lozej@gmail.com>
2009-05-05 18:32:11 +0200

    added options referer header added scan page added urlbase checking added url port checking comment removal before parsing different console output

--- chrome/content/firespider/firespider.js
+++ chrome/content/firespider/firespider.js
@@ -2,14 +2,15 @@ FBL.ns(function() { with (FBL) {
 
 var panelName = "FireSpider";
 var seen = [], togo = [], location = [], links = [];
-var qrunning = 0, requests = 0, finished = 0, request_timer = null;
+var qrunning = 0, requests = 0, finished = 0, request_timer = null, currentonly = false;
 
 Firebug.FireSpiderModel = extend(Firebug.Module,{
 
 	showPanel: function(browser, panel) {
 		var isHwPanel = panel && panel.name == panelName;
 		var hwButtons = browser.chrome.$("fbFireSpiderButtons");
-		this.start = browser.chrome.$("button_Start");
+		this.start_page = browser.chrome.$("button_Start_page");
+		this.start_site = browser.chrome.$("button_Start_site");
 		this.stop = browser.chrome.$("button_Stop");
 		this.stop.disabled = true
 		collapse(hwButtons, !isHwPanel);
@@ -21,12 +22,28 @@ Firebug.FireSpiderModel = extend(Firebug.Module,{
 		styleSheet.setAttribute("id", "hwStyles");
 		addStyleSheet(doc, styleSheet);
 	},
-
-	onStart: function(context) {
+	
+	onStart_page: function(context) {
+		currentonly = true;
 		if(context.window.location.href){
 			this.stop.disabled = false;
-			this.start.disabled = true;
-			this.logSpiderRequest('start');
+			this.start_page.disabled = true;
+			this.start_site.disabled = true;
+			this.logSpiderHeader();
+			seen = [], togo = [], location = [], links = [];
+			requests = 0, finished = 0, qrunning = 0;
+			this.cancel = false;
+			this.queue(context.window.location.href);
+		}
+	},
+
+	onStart_site: function(context) {
+		currentonly = false;
+		if(context.window.location.href){
+			this.stop.disabled = false;
+			this.start_page.disabled = true;
+			this.start_site.disabled = true;
+			this.logSpiderHeader();
 			seen = [], togo = [], location = [], links = [];
 			requests = 0, finished = 0, qrunning = 0;
 			this.cancel = false;
@@ -36,11 +53,12 @@ Firebug.FireSpiderModel = extend(Firebug.Module,{
 
 	onStop: function(context) {
 		this.stop.disabled = true;
-		this.start.disabled = false;
+		this.start_page.disabled = false;
+		this.start_site.disabled = false;
 		this.cancel = true;
 		if ( this.request.readyState > 0 || this.request.readyState < 4 ) this.request.abort();
 		clearTimeout(this.timeoutId);
-		this.logSpiderRequest('stop');
+		this.logSpiderRequest({ctype:'...',url:'stop'});
 	},
 
 	onClear: function(context) {
@@ -50,50 +68,90 @@ Firebug.FireSpiderModel = extend(Firebug.Module,{
 	reader:function(url){
 		var self = this;
 		requests++;
-		this.logSpiderRequest(url + ' ...');
+		this.logSpiderRequest({ctype:'...','url':url});
 		seen.push(url);
 		this.request = new XMLHttpRequest();
 		this.timeoutId = setTimeout( function() { // time out
 			if ( self.request.readyState > 0 || self.request.readyState < 4 ) {
 				self.request.abort();
 				finished++;
-				self.logSpiderData({ 'url':url, 'error': 'time out' ,'timer':'', 'title':'', 'where':location[url], 'link':links[url] });
+				self.logSpiderRequest({
+					ctype: self.request.status,
+					'url': url,
+					error: 'time out' ,
+					timer: '',
+					title: '',
+					where: location[url],
+					link: links[url]
+				});
 			}
 		}, 10000 );
 		self.request.onreadystatechange = function() {
 			var type = self.request.getResponseHeader("Content-Type");
 			if(type!=null && !type.match('text')) { // not text
 				clearTimeout(self.timeoutId);
-				self.logSpiderData({ 'url':url, 'error': 'not text/html' ,'timer':'', 'title':'', 'where':location[url], 'link':links[url] });
+				self.logSpiderRequest({
+					ctype: type,
+					'url': url,
+					error: 'not text/html' ,
+					timer: '',
+					title: '',
+					where: location[url],
+					link: links[url] 
+				});
 				finished++;
 				this.onreadystatechange=null;
 			}
 			if (self.request.readyState == 4 && self.request.status == 200) { // found
 				clearTimeout(self.timeoutId);
-				self.parser(url,self.request.responseText);
+				self.parser(url,self.request.responseText,type);
 			}
 
 			if (self.request.readyState > 1 && self.request.status == 404) { // not found
 				clearTimeout(self.timeoutId);
-				self.logSpiderData({ 'url':url, 'error': 'not found' ,'timer':'', 'title':'', 'where':location[url], 'link':links[url] });
+				self.logSpiderRequest({ 
+					ctype: self.request.status,
+					'url': url,
+					error: 'not found' ,
+					timer: '',
+					title: '',
+					where: location[url],
+					link: links[url] 
+				});
 				finished++;
 				this.onreadystatechange=null;
 			}
 		}
 		this.request.open("GET", url, true);
+		if (Firebug.getPref(Firebug.prefDomain,'firespider.refererheader')) this.request.setRequestHeader("Referer",location[url]);
 		request_timer = new Date();
 		this.request.send(null);
 	},
 	
-	parser: function(url,html){
+	parser: function(url,html,type){
+		html = html.replace(/<!(?:--[\s\S]*?--\s*)?>\s*/g,''); // replace comments
 		request_timer = ((new Date()).getTime() - request_timer.getTime())/1000;
-		var title = /<title>([^<]*)<\/title>/.exec(html);
-		title = (title && title!='undefined') ? title : ' ';
+		var title = /<title>([^<]*)<\/title>/i.exec(html);
+		title = title ? title : ' ';
 		var base = /<base href="(.*?)"/i.exec(html);
-		var regex = /<a[^>]+href\s*=\s*("|\')([^"|\']+)[^>]*>(.*?)<\/a>/g;
+		base = base ? base[1] : '';
+		var regex = /<a[^>]+href\s*=\s*("|\')([^"|\']+)[^>]*>(.*?)<\/a>/ig;
 		var suburl;
+		if(currentonly && finished>=1){
+			this.logSpiderRequest({
+				ctype: type,
+				'url': url,
+				error: '' ,
+				timer: request_timer.toFixed(2)+'s',
+				title: html_entity_decode(title[1]),
+				where: location[url],
+				link: links[url] 
+			});
+			finished++;
+			return;
+		}
 		while ((suburl = regex.exec(html)) != null) {
-			var tmpurl = this.makeurl(url,suburl[2]);
+			var tmpurl = this.makeurl(url,base,suburl[2]);
 			if(tmpurl && seen.indexOf(tmpurl)==-1 && togo.indexOf(tmpurl)==-1) {
 				togo.push(tmpurl);
 				location[tmpurl] = url;
@@ -101,19 +159,28 @@ Firebug.FireSpiderModel = extend(Firebug.Module,{
 				links[tmpurl] = html_entity_decode(ll ? ll : suburl[3]);
 			}
 		}
-		this.logSpiderData({ 'url':url, 'error': '' ,'timer':request_timer.toFixed(2)+'s', 'title':html_entity_decode(title[1]), 'where':location[url], 'link':links[url] })
+		this.logSpiderRequest({
+			ctype: type,
+			'url': url,
+			error: '' ,
+			timer: request_timer.toFixed(2)+'s',
+			title: html_entity_decode(title[1]),
+			where: location[url],
+			link: links[url] 
+		});		
 		finished++;
 	},
 
-	makeurl: function(url,suburl){
+	makeurl: function(url,base,suburl){
 		suburl = html_entity_decode(suburl);
-		if(suburl.match(/mailto\:|javascript\:/)) return;
+		if(suburl.match(/mailto\:|javascript\:/i)) return;
 		if(suburl.match('#')) suburl=suburl.split('#')[0];
 		if(!suburl) return;
 		var url_ = parseUri(url);
-		var domain = url_.protocol+'://'+url_.host;
+		var suburl_ = parseUri(suburl);
+		var domain = url_.protocol+'://'+url_.host+(url_.port ? (':'+url_.port) : '');
 		if(suburl.match('\:\/\/')){
-			if(suburl.match(domain)) return suburl;
+			if(suburl_.host==url_.host) return suburl;
 			else return;
 		}
 		if(suburl[0]=='/') return domain+suburl;
@@ -122,13 +189,14 @@ Firebug.FireSpiderModel = extend(Firebug.Module,{
 			return url+suburl;
 		}
 		if(suburl.match(/^\.\//)) suburl = suburl.replace(/^\.\//,'');
-		if(suburl.match(/\.\./)) {
+		if(suburl.match(/^\.\./)) {
 			url = trim(url.substr(0,strrpos(url,'/')+1),'/');
 			var c = substr_count(suburl, '..');
 			for(var i=0;i<c;i++) url = url.substr(0,strrpos(url,'/'));
 			suburl = trim(suburl,'./');
 			return url+(suburl ? '/'+suburl : '');
 		}
+		if(suburl[0]!='/' && base) return base+suburl;
 		if((url.substr(-1)!='/' && suburl[0]!='/')) return url.substr(0,strrpos(url,'/')+1)+suburl;
 		return trim(url,'/')+'/'+suburl;
 	},
@@ -139,8 +207,9 @@ Firebug.FireSpiderModel = extend(Firebug.Module,{
 		if(!start && requests==finished) {
 			if(finished && !togo.length) {
 				this.stop.disabled = true;
-				this.start.disabled = false;
-				this.logSpiderRequest('finish (parsed '+seen.length+' links)');
+				this.start_page.disabled = false;
+				this.start_site.disabled = false;
+				this.logSpiderRequest({ctype:'...',url:seen.length+' links found'});
 				return;
 			}
 			this.reader(togo.shift());
@@ -150,67 +219,116 @@ Firebug.FireSpiderModel = extend(Firebug.Module,{
 		qrunning = true;
 	},
 
-	logSpiderRequest: function(url){
+	logSpiderHeader: function(){
 		var panel = FirebugContext.getPanel('FireSpider');
-		output.request.append({'url':url}, panel.panelNode, null);
+		if(!getElementByClass(panel.panelNode, "spidertable")) output.table.append({}, panel.panelNode, null);
+		var tbody = getElementByClass(panel.panelNode, "spidertable");
+		output.header.insertRows({}, tbody.lastChild ? tbody.lastChild : tbody);
 		scrollToBottom(panel.panelNode);
 	},
-
-	logSpiderData: function(data){
-		data.ttitle = data.error ?  '' : 'title: ';
+	
+	logSpiderRequest: function(data){
 		var panel = FirebugContext.getPanel('FireSpider');
-		var len = panel.panelNode.childNodes.length-1;
-		panel.panelNode.removeChild(panel.panelNode.childNodes[len]);
-		output.requestData.append(data, panel.panelNode, null);
+		var tbody = getElementByClass(panel.panelNode, "spidertable");
+		if(data.ctype=='...'){
+			output.loading.insertRows({'url':data.url}, tbody.lastChild ? tbody.lastChild : tbody);
+		} else {
+			var len = tbody.childNodes.length-1;
+			tbody.removeChild(tbody.childNodes[len]);
+			var bg_color = len%2 ? 'row':'';
+			output.row.insertRows({
+				bgcolor:bg_color,
+				error:data.error,
+				ctype:data.ctype,
+				url:data.url,
+				title:data.title,
+				where: data.where,
+				link: data.link,
+				timer: data.timer
+			}, tbody.lastChild ? tbody.lastChild : tbody);
+		}
+		scrollToBottom(panel.panelNode);
 	}
 });
-
 
 var output = domplate({
-	'request': DIV({class: "spiderRequest"},
-		IMG({src: "chrome://firespider/content/open.png", align:'absmiddle'}),
-		"$url"
+	table: TABLE({width:'100%'},TBODY({class:'spidertable'})),
+	header: TR({class:"header"},
+		TH({width:'50'},"content"),
+		TH({align:'left'},"url"),
+		TH({align:'left'},"title"),
+		TH({align:'left'},"referer"),
+		TH({align:'right',width:'40'},"time")
 	),
-	'requestData': DIV({class: "spiderData"},
-		SPAN({class:'spiderTimer'},"$timer"),
-		IMG({src: "chrome://firespider/content/closed.png", align:'absmiddle',onclick: "$onOpenClose"}),
-		SPAN({class:'spiderUrl',onclick: "$onOpenUrl"},"$url"),
-		SPAN({class:'spiderError'},"$error"),
-		SPAN({class:'spiderFrom'},"$ttitle "),
-		SPAN({class:'spiderTitle'},"$title"),
-		DIV({class: "spiderDataMore",style:'display:none'},
-			SPAN({class:'spiderFrom'},"got from: "),
-			SPAN({class:'spiderFromWhere',onclick: "$onOpenFrom"},"$where"),
-			SPAN({class:'spiderLink'}," link name:"),
-			SPAN({class:'spiderFromLink'},"$link")
-		)
+	loading: TR(
+		TD({'align':'center'},"..."),
+		TD({'align':'left'},"$url|shorten")
 	),
-	'onOpenUrl' : function(event) {
-		openNewTab(event.target.innerHTML);
+	row: TR({class:"$bgcolor"},
+		TD({class:"$ctype|ctypecolor"},"$ctype|contenttype"),
+		TD({align:'left',onclick:"$onOpenUrl",class:'link',title:"$url"},"$url|shorten"),
+		TD({align:'left',class:"$error|errorcolor"},"$title"),
+		TD({align:'left',title:"$where",onclick:"$onOpenUrl|defined",class:'link'},"$link|defined"),
+		TD({align:'right',class:'gray'},"$timer")
+	),
+	contenttype:function(ctype){
+		return (typeof ctype =='string') ? ctype.split(/[; ]/)[0] : ctype;
 	},
-	'onOpenFrom' : function(event) {
-		openNewTab(event.target.innerHTML);
+	defined: function(){
+		//alert(typeof arguments[0])
+		return arguments[0] ? arguments[0] : '';
 	},
-	'onOpenClose' : function(event) {
-		event.target.src = event.target.src.match('open') ? event.target.src.replace('open','closed') : event.target.src.replace('closed','open');
-		var child = event.target.parentNode.getElementsByTagName('DIV')[0];
-		child.style.display = child.style.display.match('none') ? '' : 'none';
+	errorcolor: function(error) {
+		return (error) ? 'red' : '';
+	},
+	ctypecolor: function(ctype) {
+		return ctype.toString().match(/[a-z]/) ? 'green' : 'red';
+	},
+	shorten: function(){
+		var url_ = parseUri(arguments[0]);
+		var domain = url_.protocol+'://'+url_.host+(url_.port ? (':'+url_.port) : '');
+		var out = arguments[0].replace(domain,'');
+		return out ? out : '/';
+	},
+	onOpenUrl: function(event) { 
+		openNewTab(event.target.title); 
 	}
 });
 
+Firebug.bindFixed = function(){
+	var args = cloneArray(arguments), fn = args.shift(), object = args.shift();
+	return function() { return fn.apply(object, args); }
+};
 
 function FireSpiderPanel() {}
 FireSpiderPanel.prototype = extend(Firebug.Panel,{
 	name: panelName,
 	title: "FireSpider",
-
 	initialize: function() {
 		Firebug.Panel.initialize.apply(this, arguments);
 		Firebug.FireSpiderModel.addStyleSheet(this.document);
 	},
+	getOptionsMenuItems: function(context){
+		return [
+			this.optionMenu("Referer header", "firespider.refererheader"),
+			"-"
+		];
+	},
+	optionMenu: function(label, option){
+		var value = Firebug.getPref(Firebug.prefDomain, option);
+		return {
+			label: label,
+			nol10n: true,
+			type: "checkbox",
+			checked: value,
+			command: bindFixed(Firebug.setPref, this, Firebug.prefDomain, option, !value)
+		};
+	}
 });
 
 Firebug.registerModule(Firebug.FireSpiderModel);
 Firebug.registerPanel(FireSpiderPanel);
 
 }});
+
+
---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,1 @@
+pref("extensions.firebug.firespider.refererheader", false);
